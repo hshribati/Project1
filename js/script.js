edc4f5e51c3f715a7cc2a0054cd2bea6ae1121cb
hshribati <158009247+hshribati@users.noreply.github.com>
2025-03-04 14:52:48 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,12 +3,11 @@ document.addEventListener("DOMContentLoaded", function () {
   const confirmationMessage = document.getElementById("confirmation-message");
 
   form.addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevents form from actually submitting
+    event.preventDefault(); 
 
     confirmationMessage.textContent = "Thank you for signing up for our Qur'an class!";
     confirmationMessage.style.display = "block";
 
-    // Optionally, clear the form after submission
     form.reset();
   });
 });
